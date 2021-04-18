6b748ac95d50efa7cbc18123c27cceb7caff99a1
TDM <76955910+TDM-dev2@users.noreply.github.com>
2021-04-18 08:55:11 +0530

    add more explanation to code

--- backup-01/working_1.dart
+++ backup-01/working_1.dart
@@ -113,14 +113,49 @@ class JsonWidget extends StatelessWidget {
   }
 }
 
-//------------------------------------------------------------------------------
-class Selector {
-  static Widget child(Map<String, dynamic> child) {
-    //************************************child has Map */
-    //====================*********************************************************child
+
+class WidgetSelector {
+    /*
+      ------------------------------------------------child map-------------------------------------------------------------------------
+      "child" : 
+      //child is a map (widget).
+      //In here child is Column widget.
+      //child (Column)start
+        {
+          "widget" : "Column",
+          "children" : [
+            {                       
+              "widget" : "Text",
+              "text" : "Hello"
+            },
+            {
+              "widget" : "Column",
+              "children" : [
+                {                       
+                  "widget" : "Text",
+                  "text" : "Hello"
+                },
+                {
+                  "widget" : "Text",
+                  "text" : "Hi"
+                }                             
+              ] 
+            }                  
+          ]
+        }
+      //child end
+    */
+  static Widget child(Map<String, dynamic> child) { 
+    /*
+    select child type
+    child['widget'] : 'Column'   ->   JsonColumn
+    */
     switch (child['widget']) {
-      //************************************************************************ */ 'widget'   never change
       case 'Text':
+        /*
+      child (column) map is pass for all the widget
+      
+      */
         return JsonText(child);
         break;
       case 'Align':
@@ -136,29 +171,109 @@ class Selector {
         return nil;
     }
   }
+//------------------------------------------------------------------------------------------------------------------------------------
+    /*
+      Example code-------------------
+      "children" : [
+            {                             //child-1
+              "widget" : "Text",
+              "text" : "Hello"
+            },
+            {                             //child-2
+              "widget" : "Column",
+              "children" : [
+                {                       
+                  "widget" : "Text",
+                  "text" : "Hello"
+                },
+                {
+                  "widget" : "Text",
+                  "text" : "Hi"
+                }                             
+              ] 
+            }                  
+          ]
 
+        children is a list of widgets  
+     */
   static List<Widget> children(List<dynamic> children) {
-    //print(children.length);
-    //********************************************children has List */
-    //https://stackoverflow.com/questions/65144596/flutter-the-method-add-was-called-on-null
+
+//------------------------------------------------------------------------------------------------------------------------------------
     List<Widget> allChildren = [];
+    /*
+    Error:- 
+            If you try to add items for declared list -> The method '[]' was called on null.
+                                                         Receiver: null
+                                                         Tried calling: []("widget")
+    Reason:- 
+            List<Widget> allChildren;       ->  Only declare a variable with type list.
+                                                This is a empty variable (no list assign for that variable).
+            List<Widget> allChildren = [];  ->  Declare variable & store list object.
+                                                Now this variable contains a list object,so we can do list method operation on that
+                                                    eg:- .add,.length   etc
+    Solved:- 
+            Assign list object for that variable.
+    */
+    //https://stackoverflow.com/questions/65144596/flutter-the-method-add-was-called-on-null
+//
+//
+//-------------------------------------------------------------------------------------------------------------------------------------
+    
     for (int i = 0; i < children.length; i++) {
+      /*
+      Select one item() from list (List<dynamic>) and pass it to child() function
+      ex:-
+      children[1]   =  one child(map/widget) in the list.
+      */
       Widget aChild = child(children[i]);
       allChildren.add(aChild);
     }
+    /*
+    Return list of widgets  [widget1,widget2....]
+    */
     return allChildren;
   }
 }
 
-//------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------
 class JsonContainer extends StatelessWidget {
+
+  /*
+  map variable =  {
+                    "widget" : "Container",
+                    "child" : {
+                      "widget" : "Column",
+                      "children" : [
+                        {                       
+                          "widget" : "Text",
+                          "text" : "Hello"
+                        },
+                        {
+                          "widget" : "Column",
+                          "children" : [
+                            {                       
+                              "widget" : "Text",
+                              "text" : "Hello"
+                            },
+                            {
+                              "widget" : "Text",
+                              "text" : "Hi"
+                            }                             
+                            ] 
+                        }                  
+
+                      ]
+                    }
+                }
+  */
   JsonContainer(this.map);
   final Map<String, dynamic> map;
 
   @override
   Widget build(BuildContext context) {
-    //print(map['child']);
-    Widget child = Selector.child((map['child']));
+    //properties are first assign for variables before composition
+    //eg:-  child, decoration, width
+    Widget child = WidgetSelector.child((map['child']));
     return Container(
       width: 100,
       height: 100,
@@ -190,7 +305,7 @@ class JsonColumn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> sChildren = Selector.children(map['children']);
+    List<Widget> sChildren = WidgetSelector.children(map['children']);
     print(sChildren);
     return Column(
       children: sChildren,
@@ -199,14 +314,7 @@ class JsonColumn extends StatelessWidget {
 }
 
 //
-//
-//
-//
-//
-//
-//
-//
-//
+
 class JsonAlign extends StatelessWidget {
   JsonAlign(
     Map<String, dynamic> properties,
